ed5ac632897a83573bd88c1feece5508dd17db70
prassupothula <108472580+prassupothula@users.noreply.github.com>
2022-07-22 20:23:59 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes
+{
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static String n;
+	static boolean t;
+	public static void main(String args[])
+	{
+		System.out.println("Default values are: ");
+		System.out.println("byte= " + b);
+		System.out.println("short= " + s);
+		System.out.println("int= " + i);
+		System.out.println("long= " + l);
+		System.out.println("float= " + f);
+		System.out.println("double= " + d);
+		System.out.println("char= " + c);
+		System.out.println("string= " + n);
+		System.out.println("boolean= " + t);
+	}
+}
