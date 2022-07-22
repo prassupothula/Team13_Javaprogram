aed00239f91b30a7c0a3529f63fd40db01eec46c
prassupothula <108472580+prassupothula@users.noreply.github.com>
2022-07-22 20:27:56 +0530

    Add files via upload

---
+++ ArrayReverse .java
@@ -0,0 +1,8 @@
+class ArrayReverse {
+    public static void main(String args[]) {
+        int a[]={1,2,3,4};
+        for (int i=4;i>=1;i--) {
+            System.out.println(a[i]);
+        }
+    }
+}
