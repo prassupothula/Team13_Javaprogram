36d1401e6efb174f50014a6b5974cfa187ca0dbe
prassupothula <108472580+prassupothula@users.noreply.github.com>
2022-07-23 14:47:42 +0530

    unit1 test prgrms

--- Demo.java
+++ Demo.java
@@ -1,7 +1,40 @@
- public class Demo
+class Animal
 {
-    public static void main(String[] args)
-    {
-        system.out.print("Prasanna");
-    }
+	void eat()
+	{
+		System.out.println("Eating....");
+	}
 }
+class Dog extends Animal
+{
+	void bark()
+	{
+		System.out.println("Barking....");
+	}
+}
+class BabyDog extends Dog
+{
+	void sleep()
+	{
+		System.out.println("Sleeping....");
+	}
+}
+class Cat extends Animal
+{
+	void print()
+	{
+		System.out.println("I am cat");
+	}
+}
+public class Demo
+{
+	public static void main(String args[])
+	{
+		BabyDog bd=new BabyDog();
+		bd.sleep();
+		bd.bark();
+		bd.eat();
+		Cat c=new Cat();
+		c.print();
+	}
+}	
